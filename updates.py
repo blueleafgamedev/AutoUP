aaaaeadbadce233a1af03c9c2d1ad88f6b8c97c3
Travis Engh <engh.travis@outlook.com>
2022-06-09 14:13:55 -0500

    update spacing

--- updates.py
+++ updates.py
@@ -13,10 +13,12 @@ while True:
         break
 
     elif today == 1 and 'available' in checkup:
-        answer = input(emoji.emojize('Would you like me to perform your monthly maintenance? :thinking_face: [Y/n] ')).lower().strip()
+        answer = input(emoji.emojize('Would you like me to perform your monthly maintenance? '
+                                     ':thinking_face: [Y/n] ')).lower().strip()
             
         if answer == 'y' or answer == 'yes':
-            print(emoji.emojize("Please Enter Your Password and I'll get you up to speed! :beaming_face_with_smiling_eyes:\n"))
+            print(emoji.emojize("Please Enter Your Password and I'll get you up to speed! "
+                                ":beaming_face_with_smiling_eyes:\n"))
             os.system('sudo dnf -y up')
             os.system('sudo dnf -y autoremove')
             os.system('sudo dnf clean all')
@@ -28,14 +30,15 @@ while True:
 
             else:
                 print(restart)
-                answer = (emoji.emojize('It looks like some updates require a restart. Would you like me to do this? :thinking_face: [Y/n] ')).lower().strip()
+                answer = (emoji.emojize('It looks like some updates require a restart. Would you like me to do this? '
+                                        ':thinking_face: [Y/n] ')).lower().strip()
                 if answer == 'y' or answer == 'yes':
                     os.system('sudo shutdown -r 1')
                     print(emoji.emojize('System will reboot in 1 Minute. See you soon! :grinning_face_with_big_eyes:\n'))
                     break
                 
                 elif answer == 'n' or answer == 'no':
-                    print(emoji.emojize('\nAlrighty! You have a great day! :grinning_face_with_big_eyes:'))
+                    print(emoji.emojize('\nAlright! You have a great day! :grinning_face_with_big_eyes:'))
                     break
 
                 else:
@@ -52,10 +55,12 @@ while True:
     
     elif 'available' in checkup:
         print(checkup)
-        answer = input(emoji.emojize('The above updates are available. Would you like me to run them? :thinking_face: [Y/n] ')).lower().strip()
+        answer = input(emoji.emojize('The above updates are available. Would you like me to run them? :thinking_face: '
+                                     '[Y/n] ')).lower().strip()
 
         if answer == 'y' or answer == 'yes':
-            print(emoji.emojize("Please Enter Your Password and I'll get you up to speed! :beaming_face_with_smiling_eyes:\n"))
+            print(emoji.emojize("Please Enter Your Password and I'll get you up to speed! "
+                                ":beaming_face_with_smiling_eyes:\n"))
             os.system('sudo dnf -y up')
             
             restart = os.popen('dnf needs-restarting -r').read()
@@ -65,7 +70,8 @@ while True:
 
             else:
                 print(restart)
-                answer = (emoji.emojize('It looks like some updates require a restart. Would you like me to do this? :thinking_face: [Y/n] ')).lower().strip()
+                answer = (emoji.emojize('It looks like some updates require a restart. Would you like me to do this? '
+                                        ':thinking_face: [Y/n] ')).lower().strip()
                 if answer == 'y' or answer == 'yes':
                     os.system('sudo shutdown -r 1')
                     print(emoji.emojize('System will reboot in 1 Minute. See you soon! :grinning_face_with_big_eyes:\n'))
